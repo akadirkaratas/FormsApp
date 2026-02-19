e7f9c13fd7f446c2dda6338d51342ff3c9c581f0
akadirkaratas <a.kadir.karatass@gmail.com>
2026-02-19 16:41:32 +0300

    edit sayfasi eklendi ve ilgili product tasindi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -65,10 +65,14 @@ public class HomeController : Controller
 
         if (ModelState.IsValid)
         {
-            using(var stream =new FileStream(path, FileMode.Create))
+            if(imageFile != null)
             {
-                await imageFile.CopyToAsync(stream);
+                using(var stream =new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
             }
+            
              model.Image= randomFileName;
              model.ProductId= Repository.Products.Count+1;
              Repository.CreateProduct(model);
@@ -76,6 +80,23 @@ public class HomeController : Controller
         }
         ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name") ;
         return View(model);
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var entity = Repository.Products.FirstOrDefault(p => p.ProductId== id);
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name") ;
+        return View(entity);
     }
 
   
